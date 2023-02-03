7960a7cf1baac40a1ecb64a0e24435937580469e
natai <satzin0521@gmail.com>
2023-02-03 19:13:25 +0900

    rm noice

--- nvim/after/plugin/noice.rc.lua
+++
@@ -1,7 +0,0 @@
-local status, noice = pcall(require, 'noice')
-if (not status) then return end
-
-if not (vim.fn.exists('g:goneovim') == 1 or string.find(vim.v.servername, 'localhost:') ~= nil ) then
-  noice.setup({})
-end
-
