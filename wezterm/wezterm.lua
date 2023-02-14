3f39af4cf872d63933c7ab91703d8b0f9a88b04a
natai <satzin0521@gmail.com>
2023-02-15 08:31:18 +0900

    wezterm font change

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,42 +1,45 @@
-local wezterm = require('wezterm')
-local utils = require('utils')
-local keybind = require('keybind')
+local wezterm = require("wezterm")
+local utils = require("utils")
+local keybind = require("keybind")
 
 local launch_menu = {}
 
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  table.insert(launch_menu, {
-    label = 'PowerShell',
-    args = { 'pwsh.exe', '-NoLogo' }
-  })
-  table.insert(launch_menu, {
-    label = 'Builtin PowerShell',
-    args = { 'powershell.exe', '-NoLogo' }
-  })
-  for idx, domain in ipairs(wezterm.default_wsl_domains()) do
-    table.insert(launch_menu, {
-      label = domain['name'],
-      args = {
-        'wsl.exe', '--distribution', domain['distribution']
-      }
-    })
-  end
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	table.insert(launch_menu, {
+		label = "PowerShell",
+		args = { "pwsh.exe", "-NoLogo" },
+	})
+	table.insert(launch_menu, {
+		label = "Builtin PowerShell",
+		args = { "powershell.exe", "-NoLogo" },
+	})
+	for idx, domain in ipairs(wezterm.default_wsl_domains()) do
+		table.insert(launch_menu, {
+			label = domain["name"],
+			args = {
+				"wsl.exe",
+				"--distribution",
+				domain["distribution"],
+			},
+		})
+	end
 end
 
-local colors, metadata = wezterm.color.load_base16_scheme('C:/Users/natai/.dotfiles/colors/sonokai.yaml')
+local colors, metadata = wezterm.color.load_base16_scheme("C:/Users/natai/.dotfiles/colors/sonokai.yaml")
 return {
-  font = wezterm.font_with_fallback {
-    "HackGen35 Console NF",
-  },
-  use_ime = true,
-  font_size = 13.0,
-  -- color_scheme = "Sonokai (Gogh)",
-  colors = colors,
-  window_background_opacity = 1.0,
-  adjust_window_size_when_changing_font_size = false,
-  -- disable_default_key_bindings = true,
-  default_prog = { 'pwsh.exe', '-NoLogo' },
-  leader = { key = '\\', mods = 'ALT', timeout_milliseconds = 1000 },
-  keys = keybind.create_keybinds(),
-  launch_menu = launch_menu
+	font = wezterm.font_with_fallback({
+		"FiraCode NF",
+		"HackGen35 Console NF",
+	}),
+	use_ime = true,
+	font_size = 13.0,
+	-- color_scheme = "Sonokai (Gogh)",
+	colors = colors,
+	window_background_opacity = 1.0,
+	adjust_window_size_when_changing_font_size = false,
+	-- disable_default_key_bindings = true,
+	default_prog = { "wsl.exe" },
+	leader = { key = "\\", mods = "ALT", timeout_milliseconds = 1000 },
+	keys = keybind.create_keybinds(),
+	launch_menu = launch_menu,
 }
