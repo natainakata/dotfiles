42bbdb29e8cbfc5bf310f6b0094c2d5319f23a5a
natai <satzin0521@gmail.com>
2023-02-19 14:44:28 +0900

    color settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,5 @@ require("natai.options")
 require("natai.keymaps")
 require("natai.autocmd")
 require("natai.lazy")
-vim.cmd.colorscheme("base16sonokai")
-for _, v in ipairs(vim.loop.os_uname()) do
-  print(v)
-end
+vim.cmd.colorscheme("duskfox")
+
