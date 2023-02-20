a82d56362547e7b982b675e25d7b17e7d4dfd5cf
natai <satzin0521@gmail.com>
2023-02-20 10:38:22 +0900

    neovide alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -35,4 +35,4 @@ alias diff='diff -U1'
 alias fzc='fzf --preview "bat --style=numbers --color=always --line-range :500 {}"'
 
 alias vidot="vi $HOME/.dotfiles"
-alias neovide='(){/mnt/c/Users/natai/scoop/shims/neovide.exe --multigrid --wsl $@ &}'
+alias nvde='(){/mnt/c/Users/natai/scoop/shims/neovide.exe --multigrid --wsl $@ &}'
