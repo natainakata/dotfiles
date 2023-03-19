3ef4c49227641940d214e1e9129a667d3992c179
natai <satzin0521@gmail.com>
2023-03-19 16:20:40 +0900

    telescope settings separate

---
+++ nvim/lua/natai/plugins/telescope/init.lua
@@ -0,0 +1,110 @@
+local keymap = require("natai.plugins.telescope.keymaps")
+local function telescope_setup(core, actions, sorters, viewers)
+  core.setup({
+    defaults = {
+      vimgrep_arguments = {
+        "rg",
+        "-L",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+      },
+      mappings = {
+        n = {
+          ["q"] = actions.close,
+        },
+      },
+      prompt_prefix = "  ",
+      selection_caret = "  ",
+      entry_prefix = "  ",
+      selection_strategy = "reset",
+      sorting_strategy = "ascending",
+      layout_storategy = "horizontal",
+      layout_config = {
+        horizontal = {
+          prompt_position = "top",
+          preview_width = 0.5,
+          results_width = 0.6,
+        },
+        vertical = {
+          mirror = false,
+        },
+        width = 0.7,
+        height = 0.8,
+        preview_cutoff = 120,
+      },
+      file_sorter = sorters.get_fuzzy_file,
+      file_ignore_patterns = { "node_modules" },
+      generic_sorter = sorters.get_generic_fuzzy_sorter,
+      path_display = { "truncate" },
+      color_devicons = true,
+      set_env = { ["COLORTERM"] = "truecolor" },
+      file_previewer = viewers.vim_buffer_cat.new,
+      grep_previewer = viewers.vim_buffer_vimgrep.new,
+      qflist_previewer = viewers.vim_buffer_qflist.new,
+    },
+    pickers = {
+      find_files = {
+        find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
+      },
+    },
+    extensions = {
+      file_browser = {
+        theme = "cursor",
+        -- disables netrw and use telescope-file-browser in its place
+        hijack_netrw = false,
+        mappings = {
+          -- your custom insert mode mappings
+          ["i"] = {
+            ["<C-w>"] = function()
+              vim.cmd("normal vbd")
+            end,
+          },
+          ["n"] = {
+            -- your custom normal mode mappings
+            ["N"] = core.extensions.file_browser.actions.create,
+            ["h"] = core.extensions.file_browser.actions.goto_parent_dir,
+            ["/"] = function()
+              vim.cmd("startinsert")
+            end,
+          },
+        },
+      },
+    },
+  })
+  core.load_extension("file_browser")
+  core.load_extension("frecency")
+end
+
+local spec = {
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-file-browser.nvim",
+        keys = keymap.file_browser,
+      },
+      {
+        "nvim-telescope/telescope-frecency.nvim",
+        dependencies = "kkharji/sqlite.lua",
+        keys = keymap.frecency,
+      },
+      "nvim-telescope/telescope-symbols.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = keymap.core,
+    config = function()
+      local telescope = require("telescope")
+      local viewers = require("telescope.previewers")
+      local actions = require("telescope.actions")
+      local sorters = require("telescope.sorters")
+      telescope_setup(telescope, actions, sorters, viewers)
+    end,
+  },
+}
+
+return spec
---
+++ nvim/lua/natai/plugins/telescope/keymaps.lua
@@ -0,0 +1,109 @@
+local M = {}
+M.core = {
+  {
+    "<C-f>",
+    function()
+      require("telescope.builtin").find_files({
+        no_ignore = false,
+        hidden = false,
+      })
+    end,
+    desc = "Find File",
+  },
+  {
+    "<Leader><C-f>",
+    function()
+      require("telescope.builtin").find_files({
+        no_ignore = true,
+        hidden = true,
+      })
+    end,
+    desc = "Find File (ALL)",
+  },
+  {
+    "<Leader>/",
+    function()
+      require("telescope.builtin").live_grep()
+    end,
+    desc = "live grep",
+  },
+  {
+    "<Leader>b",
+    function()
+      require("telescope.builtin").buffers()
+    end,
+    desc = "Buffer List",
+  },
+  {
+    "<Leader>?",
+    function()
+      require("telescope.builtin").help_tags()
+    end,
+    desc = "Neovim Documents",
+  },
+  {
+    "<Leader>R",
+    function()
+      require("telescope.builtin").oldfiles()
+    end,
+    desc = "Recent Files",
+  },
+  {
+    "<C-g>s",
+    function()
+      require("telescope.builtin").git_status()
+    end,
+    desc = "Git Status",
+  },
+  {
+    "<C-g>b",
+    function()
+      require("telescope.builtin").git_branches()
+    end,
+    desc = "Git Branches",
+  },
+  {
+    "<C-g>C",
+    function()
+      require("telescope.builtin").git_commits()
+    end,
+    desc = "Git Commits",
+  },
+  {
+    "<C-S>",
+    function()
+      require("telescope.builtin").symbols()
+    end,
+    desc = "Symbols",
+    mode = "i",
+  },
+}
+M.file_browser = {
+  {
+    "<Leader>f",
+    function()
+      require("telescope").extensions.file_browser.file_browser({
+        path = vim.fn.getcwd(),
+        cwd = vim.fn.getcwd(),
+        respect_gitignore = false,
+        hidden = true,
+        grouped = true,
+        previewer = false,
+        initial_mode = "normal",
+        layout_config = { height = 0.5 },
+      })
+    end,
+    desc = "File Browser",
+  },
+}
+M.frecency = {
+  {
+    "<Leader>r",
+    function()
+      require("telescope").extensions.frecency.frecency({})
+    end,
+    desc = "File Frecency",
+  },
+}
+
+return M
