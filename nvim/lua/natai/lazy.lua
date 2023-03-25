7b4a548003feea1a0ddce803fe9f78be1e5de80b
natai <satzin0521@gmail.com>
2023-03-25 10:08:31 +0900

    install window colorscheme

--- nvim/lua/natai/lazy.lua
+++ nvim/lua/natai/lazy.lua
@@ -31,7 +31,7 @@ local opts = {
     patterns = {},
   },
   checker = { enabled = true, frequency = 1600 }, -- automatically check for plugin updates
-  install = { colorscheme = { "onedark", "nightfox", "habamax" } },
+  install = { colorscheme = { "onedark", "habamax" } },
 }
 
 utils.ensure("lazy", function(m)
