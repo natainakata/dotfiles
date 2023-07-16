30986d5a00c94eec89cec0448d9fea3feb33a68d
natai <satzin0521@gmail.com>
2023-07-16 11:50:06 +0900

    fix stylua and default prog

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,131 +1,131 @@
 local status, wezterm = pcall(require, "wezterm")
 if not status then
-	return
+  return
 end
 local utils = require("utils")
 local keybind = require("keybind")
 
 wezterm.on("update-status", function(window, pane)
-	local wday = os.date("*t").wday
-	local wday_ja = string.format("(%s)", utils.day_of_week_ja(wday))
-	local date = wezterm.strftime("󰃮  %Y-%m-%d " .. wday_ja .. " 󰥔  %H:%M:%S")
+  local wday = os.date("*t").wday
+  local wday_ja = string.format("(%s)", utils.day_of_week_ja(wday))
+  local date = wezterm.strftime("󰃮  %Y-%m-%d " .. wday_ja .. " 󰥔  %H:%M:%S")
 
-	local bat = ""
+  local bat = ""
 
-	for _, b in ipairs(wezterm.battery_info()) do
-		local battery_state_of_charge = b.state_of_charge * 100
-		local battery_icon = ""
-		if battery_state_of_charge >= 80 then
-			battery_icon = "80  "
-		elseif battery_state_of_charge >= 70 then
-			battery_icon = "70  "
-		elseif battery_state_of_charge >= 60 then
-			battery_icon = "60  "
-		elseif battery_state_of_charge >= 50 then
-			battery_icon = "50  "
-		elseif battery_state_of_charge >= 40 then
-			battery_icon = "40  "
-		elseif battery_state_of_charge >= 30 then
-			battery_icon = "30  "
-		elseif battery_state_of_charge >= 20 then
-			battery_icon = "20  "
-		else
-			battery_icon = "󱊡  "
-		end
+  for _, b in ipairs(wezterm.battery_info()) do
+    local battery_state_of_charge = b.state_of_charge * 100
+    local battery_icon = ""
+    if battery_state_of_charge >= 80 then
+      battery_icon = "80  "
+    elseif battery_state_of_charge >= 70 then
+      battery_icon = "70  "
+    elseif battery_state_of_charge >= 60 then
+      battery_icon = "60  "
+    elseif battery_state_of_charge >= 50 then
+      battery_icon = "50  "
+    elseif battery_state_of_charge >= 40 then
+      battery_icon = "40  "
+    elseif battery_state_of_charge >= 30 then
+      battery_icon = "30  "
+    elseif battery_state_of_charge >= 20 then
+      battery_icon = "20  "
+    else
+      battery_icon = "󱊡  "
+    end
 
-		bat = string.format("%s%.0f%% ", battery_icon, battery_state_of_charge)
-	end
+    bat = string.format("%s%.0f%% ", battery_icon, battery_state_of_charge)
+  end
 
-	window:set_right_status(wezterm.format({
-		{ Text = date },
-	}))
-	-- window:set_right_status(wezterm.format({
-	-- 	{ Text = date .. " " .. bat },
-	-- }))
+  window:set_right_status(wezterm.format({
+    { Text = date },
+  }))
+  -- window:set_right_status(wezterm.format({
+  --   { Text = date .. " " .. bat },
+  -- }))
 end)
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local tab_index = tab.tab_index + 1
-	if tab.is_active and string.match(tab.active_pane.title, "Copy mode:") then
-		return string.format(" %d %s ", tab_index, "Copy mode...")
-	end
-	return string.format(" %d ", tab_index)
+  local tab_index = tab.tab_index + 1
+  if tab.is_active and string.match(tab.active_pane.title, "Copy mode:") then
+    return string.format(" %d %s ", tab_index, "Copy mode...")
+  end
+  return string.format(" %d ", tab_index)
 end)
 
 local launch_menu = {}
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	table.insert(launch_menu, {
-		label = "PowerShell",
-		args = { "pwsh.exe", "-NoLogo" },
-	})
-	table.insert(launch_menu, {
-		label = "Builtin PowerShell",
-		args = { "powershell.exe", "-NoLogo" },
-	})
-	table.insert(launch_menu, {
-		label = "Neovim",
-		args = { "nvim.exe" },
-	})
-	for idx, domain in ipairs(wezterm.default_wsl_domains()) do
-		table.insert(launch_menu, {
-			label = domain["name"],
-			args = {
-				"wsl.exe",
-				"--distribution",
-				domain["distribution"],
-			},
-		})
-	end
+  table.insert(launch_menu, {
+    label = "PowerShell",
+    args = { "pwsh.exe", "-NoLogo" },
+  })
+  table.insert(launch_menu, {
+    label = "Builtin PowerShell",
+    args = { "powershell.exe", "-NoLogo" },
+  })
+  table.insert(launch_menu, {
+    label = "Neovim",
+    args = { "nvim.exe" },
+  })
+  for idx, domain in ipairs(wezterm.default_wsl_domains()) do
+    table.insert(launch_menu, {
+      label = domain["name"],
+      args = {
+        "wsl.exe",
+        "--distribution",
+        domain["distribution"],
+      },
+    })
+  end
 end
 
 local default_cwd = os.getenv("HOME")
 
 local colors = {
-	foreground = "#dcd7ba",
-	background = "#1f1f28",
+  foreground = "#dcd7ba",
+  background = "#1f1f28",
 
-	cursor_bg = "#c8c093",
-	cursor_fg = "#c8c093",
-	cursor_border = "#c8c093",
+  cursor_bg = "#c8c093",
+  cursor_fg = "#c8c093",
+  cursor_border = "#c8c093",
 
-	selection_fg = "#c8c093",
-	selection_bg = "#2d4f67",
+  selection_fg = "#c8c093",
+  selection_bg = "#2d4f67",
 
-	scrollbar_thumb = "#16161d",
-	split = "#16161d",
+  scrollbar_thumb = "#16161d",
+  split = "#16161d",
 
-	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+  ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+  brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+  indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
 }
 
 return {
-	font = wezterm.font_with_fallback({
-		{
-			family = "0xProto",
-			weight = "Regular",
-			stretch = "Normal",
-			italic = false,
-		},
-		{ family = "BIZ UDGothic", weight = "Regular", stretch = "Normal", style = "Normal" },
-	}),
-	use_ime = true,
-	font_size = 13.0,
-	initial_cols = 120,
-	initial_rows = 30,
-	-- color_scheme = "Sonokai (Gogh)",
-	color_scheme = "OneDark (base16)",
-	-- color_scheme = "kanagawa (Gogh)",
-	-- colors = colors,
-	window_background_opacity = 0.9,
-	adjust_window_size_when_changing_font_size = false,
-	-- disable_default_key_bindings = true,
-	default_prog = { "pwsh.exe" },
-	default_cwd = default_cwd,
-	leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 },
-	keys = keybind.create_keybinds(),
-	use_fancy_tab_bar = false,
-	tab_bar_at_bottom = true,
-	launch_menu = launch_menu,
+  font = wezterm.font_with_fallback({
+    {
+      family = "0xProto",
+      weight = "Regular",
+      stretch = "Normal",
+      italic = false,
+    },
+    { family = "BIZ UDGothic", weight = "Regular", stretch = "Normal", style = "Normal" },
+  }),
+  use_ime = true,
+  font_size = 13.0,
+  initial_cols = 120,
+  initial_rows = 30,
+  -- color_scheme = "Sonokai (Gogh)",
+  color_scheme = "OneDark (base16)",
+  -- color_scheme = "kanagawa (Gogh)",
+  -- colors = colors,
+  window_background_opacity = 0.9,
+  adjust_window_size_when_changing_font_size = false,
+  -- disable_default_key_bindings = true,
+  -- default_prog = { "pwsh.exe" },
+  default_cwd = default_cwd,
+  leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 },
+  keys = keybind.create_keybinds(),
+  use_fancy_tab_bar = false,
+  tab_bar_at_bottom = true,
+  launch_menu = launch_menu,
 }
