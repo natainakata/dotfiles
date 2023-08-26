599227bb95a64e7f9614df15bce216c3b7bf288c
natai <satzin0521@gmail.com>
2023-08-26 15:35:41 +0900

    add nvim-lint

--- nvim/lua/rc/plugins/lsp/init.lua
+++ nvim/lua/rc/plugins/lsp/init.lua
@@ -87,6 +87,16 @@ local spec = {
       })
     end,
   },
+  {
+    "mfussenegger/nvim-lint",
+    config = function()
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
+  },
 }
 
 return spec
