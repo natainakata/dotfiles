20f1248052ec6a275430bff552da1da20e222ec5
natai nakata <satzin0521@gmail.com>
2023-12-30 11:31:56 +0900

    fix fisher auto setup

--- fish/config.fish
+++ fish/config.fish
@@ -11,7 +11,7 @@ rtx activate fish | source
 starship init fish | source
 
 set -U fisher_path "$HOME/.local/share/fisher"
-if test -d "$HOME/.local/share/fisher"
+if test ! -d "$HOME/.local/share/fisher"
   mkdir -p "$HOME/.local/share/fisher"
   curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
 end
