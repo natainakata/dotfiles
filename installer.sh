eea3787a705fa8a105876bc02d98b65f72e460e9
natai nakata <satzin0521@gmail.com>
2024-04-26 14:15:54 +0900

    fix aqua version

--- installer.sh
+++ installer.sh
@@ -33,12 +33,15 @@ if [ ! -d ${DOT_DIR} ]; then
   ln -snfv "$DOT_DIR/tmux.conf" "$HOME/.tmux.conf"
   ln -snfv "$DOT_DIR/bin" "$HOME/.bin"
 
-  if  has "aqua"; then
+  if has "aqua"; then
     export AQUA_GLOBAL_CONFIG="$HOME/.config/aqua/aqua.yaml"
     aqua i -l -a
-  else 
+  else
     export PATH="${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH"
-    curl -fsSL "https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.1.2/aqua-installer" | bash
+    curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.0/aqua-installer
+    echo "8299de6c19a8ff6b2cc6ac69669cf9e12a96cece385658310aea4f4646a5496d  aqua-installer" | sha256sum -c
+    chmod +x aqua-installer
+    ./aqua-installer
     export AQUA_GLOBAL_CONFIG="$HOME/.config/aqua/aqua.yaml"
     aqua i -l -a
   fi
