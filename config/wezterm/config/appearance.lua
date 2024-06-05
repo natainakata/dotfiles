75b27f1ffe991dcc0267ce6c822447f19528d5d5
natai nakata <satzin0521@gmail.com>
2024-06-06 08:05:28 +0900

    wezterm color change

--- config/wezterm/config/appearance.lua
+++ config/wezterm/config/appearance.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 
-local color_scheme = "OneDark (base16)"
+local color_scheme = "Kanagawa (Gogh) "
 local colors = wezterm.color.get_builtin_schemes()[color_scheme]
 
 return {
