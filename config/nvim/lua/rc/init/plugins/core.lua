d2112add478a604216c2b551e8c3630e7edc72ea
natai nakata <satzin0521@gmail.com>
2024-06-11 08:41:16 +0900

    add neovim japanese documents

--- config/nvim/lua/rc/init/plugins/core.lua
+++ config/nvim/lua/rc/init/plugins/core.lua
@@ -1,6 +1,10 @@
 return {
-  { "folke/lazy.nvim",      version = "*" },
-  { "Olical/nfnl",          ft = "fennel" },
+  { "folke/lazy.nvim",            version = "*" },
+  { "willelz/neovimdoc-ja" },
   { "nvim-neotest/nvim-nio" },
   { "vim-denops/denops.vim" },
+  { "nvim-lua/plenary.nvim" },
+  { "tpope/vim-repeat" },
+  { "MunifTanjim/nui.nvim" },
+  { "nvim-tree/nvim-web-devicons" },
 }
