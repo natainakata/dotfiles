5d6655088bf3b580b3d8ded6d6d21dfba6333c32
natai nakata <satzin0521@gmail.com>
2024-06-11 09:32:34 +0900

    Changed due to overlap with window prefix

--- config/nvim/lua/rc/init/plugins/editor.lua
+++ config/nvim/lua/rc/init/plugins/editor.lua
@@ -10,6 +10,15 @@ return {
       { "<Leader>e", "<Cmd>NvimTreeToggle<CR>", desc = "NvimTree" },
     },
     opts = {
+      on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+        local function opts(desc)
+          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+        api.config.mappings.default_on_attach(bufnr)
+        vim.keymap.del("n", "s", { buffer = bufnr })
+        vim.keymap.set("n", "<C-o>", api.node.run.system, opts("Run System"))
+      end,
       disable_netrw = false,
       hijack_netrw = false,
       filters = {
