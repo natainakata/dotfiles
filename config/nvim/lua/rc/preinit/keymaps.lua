27e08312e684861204cd99c8ec9e54710bfcb238
natai nakata <satzin0521@gmail.com>
2024-06-17 10:30:16 +0900

    fix: 🚑️ sキーの割り当てが重複していたので修正

--- config/nvim/lua/rc/preinit/keymaps.lua
+++ config/nvim/lua/rc/preinit/keymaps.lua
@@ -5,8 +5,6 @@ local xmap = utils.xmap
 local tmap = utils.tmap
 local keymap = vim.keymap
 
-keymap.set("", "<Space>", "<Nop>")
-local opts = { silent = true }
 
 -- general keymap
 nmap("j", "gj")
@@ -22,45 +20,46 @@ nmap("<Leader>q", "<Cmd>qa<CR>")
 for _, key in ipairs({ "x", "X" }) do
   keymap.set({ "n", "x" }, key, [["_]] .. key, { desc = key .. " with blackhole register" })
 end
--- keymap.set({ "n", "x" }, "x", [["_x]], { desc = "x with blackhole register" })
--- keymap.set({ "n", "x" }, "X", [["_X]], { desc = "X with blackhole register" })
 
 -- text objects
 for _, quote in ipairs({ '"', "'", "`" }) do
   keymap.set({ "x", "o" }, "a" .. quote, "2i" .. quote)
 end
 
--- window and buffer
+-- window control
+-- prefix
+nmap("m", "<Nop>")
+
+-- quit
+nmap("mq", "<Cmd>q<CR>")
+
+-- buffer
 nmap("<S-Tab>", "<Cmd>bprevious<CR>")
 nmap("<Tab>", "<Cmd>bnext<CR>")
 nmap("<S-h>", "<Cmd>bprevious<CR>")
 nmap("<S-l>", "<Cmd>bnext<CR>")
-
--- prefix s
-nmap("s", "<Nop>", { remap = true })
-
--- quit
-nmap("sq", "<Cmd>q<CR>")
+nmap("m[", "<Cmd>bprevious<CR>")
+-- nmap("mp", "<Cmd>bprevious<CR>")
+nmap("m]", "<Cmd>bnext<CR>")
+-- nmap("mn", "<Cmd>bnext<CR>")
 
 -- window
-nmap("ss", "<Cmd>split<CR><C-w>w", opts)
-nmap("sv", "<Cmd>vsplit<CR><C-w>w", opts)
+nmap("ms", "<Cmd>split<CR><C-w>w")
+nmap("mv", "<Cmd>vsplit<CR><C-w>w")
 for _, direction in ipairs({ "h", "j", "k", "l" }) do
   nmap("<C-" .. direction .. ">", "<Cmd>wincmd " .. direction .. "<CR>", { remap = true })
-  nmap("s" .. direction, "<Cmd>wincmd " .. direction .. "<CR>", { remap = true })
+  nmap("m" .. direction, "<Cmd>wincmd " .. direction .. "<CR>", { remap = true })
 end
 nmap("<C-Up>", "<Cmd>resize +2<CR>", { remap = true })
 nmap("<C-Down>", "<Cmd>resize -2<CR>", { remap = true })
 nmap("<C-Left>", "<Cmd>vertical resize -2<CR>", { remap = true })
 nmap("<C-Right>", "<Cmd>vertical resize +2<CR>", { remap = true })
 
--- buffer and tab
-nmap("st", "<Cmd>tabnew<CR>")
-nmap("sw", "<Cmd>tabclose<CR>")
-nmap("sp", "<Cmd>bprevious<CR>")
-nmap("sn", "<Cmd>bnext<CR>")
-nmap("sP", "<Cmd>tabprevious<CR>")
-nmap("sN", "<Cmd>tabnext<CR>")
+-- tab
+nmap("mt", "<Cmd>tabnew<CR>")
+nmap("mw", "<Cmd>tabclose<CR>")
+nmap("mp", "<Cmd>tabprevious<CR>")
+nmap("mn", "<Cmd>tabnext<CR>")
 
 -- kill buffer
 vim.api.nvim_create_user_command("BufferDeleteSafety", function()
