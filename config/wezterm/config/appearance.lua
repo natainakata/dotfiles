304beafcc0a858447841072c4c0ce0777f94007e
natai nakata <satzin0521@gmail.com>
2024-06-23 10:56:46 +0900

    feat: 💄OneDark Base16

--- config/wezterm/config/appearance.lua
+++ config/wezterm/config/appearance.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 
-local color_scheme = "One Dark (Gogh)"
+local color_scheme = "One Dark (base16)"
 local colors = wezterm.color.get_builtin_schemes()[color_scheme]
 
 return {
