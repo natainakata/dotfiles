ce17c51fbeaafe99e0232b33dadbc3cf75534f65
natai nakata <satzin0521@gmail.com>
2024-06-23 10:29:18 +0900

    chore: ⬇️ neovimをstableに

--- nix/home-manager/nvim.nix
+++ nix/home-manager/nvim.nix
@@ -1,6 +1,6 @@
 {inputs, pkgs, config, ...}: {
   programs.neovim = {
     enable = true;
-    package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+    # package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
   };
 }
