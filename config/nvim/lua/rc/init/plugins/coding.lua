187982be04ea07c3c0d2bec1d5154a12b16fe396
natai nakata <satzin0521@gmail.com>
2024-06-27 09:18:13 +0900

    style: 🎨 format code

--- config/nvim/lua/rc/init/plugins/coding.lua
+++ config/nvim/lua/rc/init/plugins/coding.lua
@@ -42,8 +42,8 @@ local spec = {
             name = "nvim_lsp",
             entry_filter = function(entry)
               if
-                entry:get_kind() == require("cmp.types").lsp.CompletionItemKind.Snippet
-                and entry.source:get_debug_name() == "nvim_lsp:emmet_ls"
+                  entry:get_kind() == require("cmp.types").lsp.CompletionItemKind.Snippet
+                  and entry.source:get_debug_name() == "nvim_lsp:emmet_ls"
               then
                 return false
               end
@@ -124,7 +124,7 @@ local spec = {
         sources = cmp.config.sources({
           { name = "path" },
         }, {
-          { name = "cmdline", keyword_length = 2 },
+          { name = "cmdline",  keyword_length = 2 },
           { name = "skkeleton" },
         }),
       }
