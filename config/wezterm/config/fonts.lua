592363ed24585a56ea64e256f888084a2cc0c182
natai nakata <satzin0521@gmail.com>
2024-07-18 13:49:52 +0900

    feat: 💄change wezterm font Neon

--- config/wezterm/config/fonts.lua
+++ config/wezterm/config/fonts.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
-local font = { family = "Moralerspace Krypton NF", harfbuzz_features = { "calt=1", "liga=1", "dlig=1" } }
-local font_size = 12
+local font = { family = "Moralerspace Neon NF", harfbuzz_features = { "calt=1", "liga=1", "dlig=1" } }
+local font_size = 11
 
 return {
   font = wezterm.font(font),
