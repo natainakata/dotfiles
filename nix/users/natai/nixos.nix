a87534e0537eb9faabbbcee232dfa4806ee4baea
natai nakata <satzin0521@gmail.com>
2024-09-10 16:48:57 +0900

    chore: change default shell

--- nix/users/natai/nixos.nix
+++ nix/users/natai/nixos.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [
     inputs.xremap.nixosModules.default
@@ -8,6 +8,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFFWnIE5q4KWjMz5dI8YnZ7zg2zn3ZY0oie+1h2SAvZR satzin0521@gmail.com"
     ];
+    shell = pkgs.zsh;
   };
   services.xremap = {
     userName = "natai";
