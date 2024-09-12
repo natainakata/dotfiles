a80294c0152b87944b3bad2f0f10995f8a061968
natai nakata <satzin0521@gmail.com>
2024-09-12 09:01:45 +0900

    feat: add skk settings

--- nix/modules/fcitx5.nix
+++ nix/modules/fcitx5.nix
@@ -1,14 +1,22 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+
 {
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-mozc
       fcitx5-cskk
+      fcitx5-cskk-qt
+      fcitx5-gtk
+      libsForQt5.fcitx5-qt
     ];
   };
   environment.systemPackages = with pkgs; [
     skk-dicts
     skktools
   ];
+  services.dbus.packages = [ config.i18n.inputMethod.package ];
+  environment.variables = {
+    QT_IM_MODULE = "fcitx";
+  };
 }
