c4eb585be74f7976d6e764404815627f07a348be
natai nakata <satzin0521@gmail.com>
2024-09-13 16:37:15 +0900

    feat: add pavucontrol

--- nix/modules/system/audio.nix
+++ nix/modules/system/audio.nix
@@ -1,3 +1,4 @@
+{pkgs, ...}:
 {
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
@@ -14,4 +15,7 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+  ];
 }
