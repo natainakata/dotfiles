9283077c51023494b866489b55bdf3a7d96f58d6
natai nakata <satzin0521@gmail.com>
2024-09-19 21:27:21 +0900

    fix: input error

--- nix/flake.nix
+++ nix/flake.nix
@@ -46,7 +46,7 @@
               inherit inputs;
             };
           in
-          home-manager.lib.homeManagerConfiguration {
+          inputs.home-manager.lib.homeManagerConfiguration {
           pkgs = import inputs.nixpkgs {
             system = "x86_64-linux";
             config.allowUnfree = true;
